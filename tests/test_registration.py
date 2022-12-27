944817940e6332f72c2101f8d6119681bf3e54b6
tigran-karapetian <karapetiantm@gmail.com>
2022-12-27 14:15:08 +0200

    removed uppercase from variable names

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -11,8 +11,8 @@ def test_correct_registration(driver, email_generator):
     driver.find_element(*TestLocators.password).send_keys("12345678")
     driver.find_element(*TestLocators.registration_register_button).click()
 
-    element = driver.find_element(By.XPATH, "//html/body/div[1]/div/main/div/form/button")
-    assert driver.find_element(By.XPATH, "//html/body/div[1]/div/main/div/form/button") == element
+    element = driver.find_element(*TestLocators.enter_button_login_page)
+    assert driver.find_element (*TestLocators.enter_button_login_page) == element
 
 
 # Проверка ошибки для некорректного пароля
@@ -24,5 +24,5 @@ def test_erorr_text_for_incorrect_password(driver, email_generator):
     driver.find_element(*TestLocators.password).send_keys("1234")
     driver.find_element(*TestLocators.registration_register_button).click()
 
-    error = driver.find_element(By.XPATH, "//html/body/div[1]/div/main/div/form/fieldset[3]/div/p").text
+    error = driver.find_element(By.XPATH, ".//p[@class = 'input__error text_type_main-default']").text
     assert error == 'Некорректный пароль'
